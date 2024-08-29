acf2ee879b8178d659d5c2477158a7f3f94bbafa
ansctrwhyn <57304190+ansctrwhyn@users.noreply.github.com>
2024-08-29 10:05:35 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+        if ($this->app->environment('production')){
+            URL::forceScheme('https');
+        }
     }
 }
